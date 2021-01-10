5533429324d41930deabfbbca40dc3434796a8c4
Nick <nikitakrm87@gmail.com>
2021-01-10 19:03:36 +0200

    added script to check availability

---
+++ scripts/implement.js
@@ -0,0 +1,13 @@
+import getGoodsInStock from './AvailabilityСheck.js';
+
+const itemsArray = getGoodsInStock;
+
+const source = document.querySelector('#menu-template').innerHTML.trim();
+
+const template = Handlebars.compile(source);
+
+const markup = itemsArray.map(item => template(item)).join('');
+//   console.log(markup);
+
+const container = document.querySelector('#js-menu');
+container.innerHTML = markup;
---
+++ scripts/products.js
@@ -0,0 +1,124 @@
+const goods = [
+    {
+        "availability": 1,
+        "id": "tPR1Uhkp",
+        "name": "Качотта",
+        "titleDescription": "Традиційний італійський сир с визріванням та рожевою скоринкою.",
+        "description": "Традиційний італійський сир с визріванням та рожевою скоринкою.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Качотта.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "QhKmsMtF",
+        "name": "Карачун",
+        "titleDescription": "Авторський сир с визріванням та біло-рожевою скоринкою.",
+        "description": "Карачун - це авторський сир нашого Сировара. Виготовлений з коров'ячого молока, має приємний ніжний вершковий смак.Термін дозрівання не менше одного місяця. Добре смакує з сухими винами.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Карачун.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "6udUvSWE",
+        "name": "Скіф",
+        "titleDescription": "Традиційний французський сир с визріванням та рожевою скоринкою.",
+        "description": "Традиційний французський сир с визріванням та рожевою скоринкою.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Скіф.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "meTDfnZt",
+        "name": "Реблошон",
+        "titleDescription": "Традиційний французський сир с визріванням та біло-рожевою скоринкою.",
+        "description": "Традиційний французський сир с визріванням та біло-рожевою скоринкою.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Реблошон.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "i7yiatiB",
+        "name": "Гауда",
+        "titleDescription": "Традиційний голандський сир с визріванням.",
+        "description": "Традиційний голандський сир с визріванням.",
+        "price100": 45,
+        "price1000": 450,
+        "image": "./images/all_img/cheese/Гауда.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "XLWj2q9V",
+        "name": "Кайрфіллі",
+        "titleDescription": "Традиційний англійський сир с визріванням.",
+        "description": "Традиційний англійський сир с визріванням.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Кайрфіллі.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "-o_AMdjH",
+        "name": "Чеддер",
+        "titleDescription": "Традиційний англійський сир с визріванням та бандажом.",
+        "description": "Традиційний англійський сир с визріванням та бандажом.",
+        "price100": 50,
+        "price1000": 500,
+        "image": "./images/all_img/cheese/Чеддер.jpg"
+    },
+    {
+        "availability": 1,
+        "id": "5syIO6QA",
+        "name": "Монтеррей Джек",
+        "titleDescription": "Традиційний амеріканський сир з папрікою с визріванням.",
+        "description": "Традиційний амеріканський сир з папрікою с визріванням.",
+        "price100": 45,
+        "price1000": 450,
+        "image": "./images/all_img/cheese/Монтеррей Джек.jpg"
+    },
+    {
+        "availability": 0,
+        "id": "RzXbHILm",
+        "name": "Торський сир",
+        "titleDescription": "Свіжій ніжний молочний сир без визрівання (аналог Осетинського).",
+        "description": "Свіжій ніжний молочний сир без визрівання (аналог Осетинського).",
+        "price100": 30,
+        "price1000": 300,
+        "image": "./images/all_img/cheese/Торський сир.jpg"
+    },
+    {
+        "availability": 0,
+        "id": "4InYazF3",
+        "name": "Маасдам",
+        "titleDescription": "Традиційний голандський сир с визріванням.",
+        "description": "Традиційний голандський сир с визріванням.",
+        "price100": 45,
+        "price1000": 450,
+        "image": "./images/all_img/cheese/Маасдам.jpg"
+    },
+    {
+        "availability": 0,
+        "id": "F5S-S36-",
+        "name": "Каммамбер",
+        "titleDescription": "Традиційний французський сир с визріванням та білою скоринкою.",
+        "description": "Традиційний французський сир с визріванням та білою скоринкою.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Каммамбер.jpg"
+    },
+    {
+        "availability": 0,
+        "id": "6vFscpA-",
+        "name": "Люторс",
+        "titleDescription": "Авторський сир за принципом каммамбера з білою скоринкою.",
+        "description": "Авторський сир за принципом каммамбера з білою скоринкою.",
+        "price100": 40,
+        "price1000": 400,
+        "image": "./images/all_img/cheese/Люторс.jpg"
+    },
+];
+
+export default goods;
